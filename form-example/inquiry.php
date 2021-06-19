fbb6cc08c088cfd78282f1b4b95fcc5f0df49f98
Nobumasa Ura <ura@skuroo.net>
2021-06-19 21:11:18 +0900

    GAのタグ挿入

---
+++ form-example/inquiry.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>問い合わせフォーム</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <h1>問い合わせを受け付けました</h1>
+<pre>
+<?php
+    var_dump($_REQUEST);
+?>
+</pre>
+    </body>
+</html>
